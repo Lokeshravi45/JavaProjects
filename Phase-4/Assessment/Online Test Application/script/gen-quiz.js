757745ee88cf6692cb4d926c4445e52327a4d07c
Lokeshravi45 <Lokeshravi45@gmail.com>
2022-04-11 11:19:40 +0530

    JavaProjects

---
+++ Phase-4/Assessment/Online Test Application/script/gen-quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"Full Form of RAM?",
+        option:[
+            "1.Random access memory",
+            "2.Royal access memory",
+            "3.Random ace memory",
+            "4.Bug",
+        ],
+        answer:1,
+    },
+    {
+        question:"Which type of lens is used in camera?",
+        option:[
+            "1.Convex",
+            "2.Concave",
+            "3.Circular",
+            "4.Equal thickness",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"How many RAM slot is there in computer?",
+        option:[
+            "1.1",
+            "2.2",
+            "3.3",
+            "4.4",   
+        ],
+        answer:2,
+    },
+    {
+        question:"What is the full form of WLAN",
+        option:[
+            "1.Wide Area Network",
+            "2.Wide Area Network",
+            "3.Wave Area Network",
+            "4.None of These",   
+        ],
+        answer:2,
+
+    },
+    {
+        question:"What is the full form of LAN",
+        option:[
+            "1.Lane Area Network",
+            "2.Local Area Network",
+            "3.Large Area Network",
+            "4.None of These",   
+        ],
+        answer:2,
+    }
+]
---
+++ Phase-4/Assessment/Online Test Application/script/geo-quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"Which is the largest state in India?",
+        option:[
+            "1.Madhya Pradesh",
+            "2.Rajasthan",
+            "3.Uttar Pradesh",
+            "4.Maharashtra",
+        ],
+        answer:2,
+    },
+    {
+        question:" How many continents of the world?",
+        option:[
+            "1.24",
+            "2.7",
+            "3.3",
+            "4.4",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"Where is the Eiffel Tower located?",
+        option:[
+            "1.Niger",
+            "2.India",
+            "3.America",
+            "4.Paris",   
+        ],
+        answer:4,
+    },
+    {
+        question:"Which is the longest river in India?",
+        option:[
+            "1.Ganga",
+            "2.Yamuna",
+            "3.Narmada",
+            "4.None of These",   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"how many india states?",
+        option:[
+            "1.66",
+            "2.28",
+            "3.33",
+            "4.31",   
+        ],
+        answer:2,
+    }
+]
